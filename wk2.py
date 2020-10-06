10cc924197aca8d7c8c54084c9be95134aa1b476
mikhail-cct <tayemnyj+cct@gmail.com>
2020-10-06 18:16:29 +0000

    wk2 work

---
+++ wk2.py
@@ -0,0 +1,17 @@
+dict = [{
+    "name": "John Doe",
+    "age": 21,
+    "net worth": 5213.4
+    },
+    {
+    "name": "John Doe",
+    "age": 21,
+    "net worth": 5213.4
+    }]
+
+print(type(dict))
+print(dict)
+print(dict["net worth"])
+
+# name = "John"
+# print(type(name))
